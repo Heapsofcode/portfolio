8c539c8d9eb13b9b88065925be668e7d64ce3207
heapsofcode <paulfreemanuk@outlook.com>
2024-03-11 14:20:36 +0100

    refactors project layout

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const toggleDisplay = (id) => {
-  document.getElementById(id).style.display === "block"
+  document.getElementById(id).style.display === "flex"
     ? (document.getElementById(id).style.display = "none")
-    : (document.getElementById(id).style.display = "block");
+    : ((document.getElementById(id).style.display = "flex"),
+      (document.getElementById(id).style.height = "200px"));
 };
 
 document.getElementById("project-1").addEventListener("click", function () {
@@ -15,3 +16,21 @@ document.getElementById("project-2").addEventListener("click", function () {
 document.getElementById("project-3").addEventListener("click", function () {
   toggleDisplay("project-three-info");
 });
+
+document
+  .getElementById("project-one-info")
+  .addEventListener("click", function () {
+    toggleDisplay("project-one-info");
+  });
+
+document
+  .getElementById("project-two-info")
+  .addEventListener("click", function () {
+    toggleDisplay("project-two-info");
+  });
+
+document
+  .getElementById("project-three-info")
+  .addEventListener("click", function () {
+    toggleDisplay("project-three-info");
+  });
